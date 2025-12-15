0a7fac4e97d4171121fe173d5cfcccbe2b91178f
Robin <robindev2007@gmail.com>
2025-12-15 08:54:46 +0600

    Update

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,12 +1,45 @@
+import { openapi } from "@elysiajs/openapi";
 import { Elysia, t } from "elysia";
+import sharp from "sharp";
 
-const app = new Elysia({ prefix: "/api" })
-  .get("/", "Hello Nextjs")
-  .post("/", ({ body }) => body, {
+const app = new Elysia({ prefix: "/api" }).use(openapi());
+
+app.post(
+  "/convert",
+  async ({ body: { file } }) => {
+    const fileArrayBuffer = await file.arrayBuffer();
+
+    const convertedBuffer = await sharp(Buffer.from(fileArrayBuffer))
+      .webp()
+      .toBuffer();
+
+    // Convert Buffer → Uint8Array (Blob-compatible)
+    const uint8Array = new Uint8Array(convertedBuffer);
+
+    const newFile = new File([uint8Array], "example.webp", {
+      type: "image/webp",
+    });
+
+    return newFile;
+  },
+  {
     body: t.Object({
-      name: t.String(),
+      file: t.File(),
     }),
-  });
+  }
+);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
+
+// Source - https://stackoverflow.com/a
+// Posted by T.J, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-15, License - CC BY-SA 4.0
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "20mb",
+    },
+  },
+};
